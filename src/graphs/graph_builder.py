621dff0398734edafe8ad8b83aaa9620e3176ac3
abhinit1999 <krabhinit60@gmail.com>
2025-11-22 12:14:39 +0530

    coding done for langgraph.json

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -30,3 +30,13 @@ class GraphBuilder:
         if usecase=="topic":
             self.build_topic_graph()
         return self.graph.compile()
+    
+
+
+## Below code is for the LangSmith langGraph Studio
+llm = GroqLLM().get_llm()
+
+## Get the graph
+
+graph_builder = GraphBuilder(llm)
+graph = graph_builder.build_topic_graph().compile()
